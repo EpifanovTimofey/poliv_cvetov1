e86aa67e139cb1a3cd437cbbd5780e6d526d5132
artem <Football@gmail.com>
2023-04-20 15:39:30 +0300

    Version 4

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import pygame, random
 
-sky = 2
+sky = 10
 y = random.randint(10, 100)
 x2 = random.randint(10, 60)
 x1 = random.randint(0, 100 - x2)
@@ -8,8 +8,12 @@ rect = pygame.Rect([x1, -y, x2, y])
 
 dx = 100
 
+rectcv1 = pygame.Rect([1, 400, 50, 100])
+rectcv2 = pygame.Rect([50, 400, 50, 100])
+
+
 def pramougolnik():
-    global dx
+    global dx, visotacv1
     rect.y += sky
     if rect.top > 500:
         rect.bottom = 0
@@ -17,4 +21,13 @@ def pramougolnik():
         rect.left = random.randint(0, dx - rect.w)
         rect.h = random.randint(10, 100)
     if rect.bottom > 499:
-        dx += 1
+        dx += 8
+        rectcv1.w = dx / 2
+        rectcv1.y -= 1
+        rectcv1.h += 1
+
+        rectcv2.left = dx / 2
+        rectcv2.w = dx / 2
+        rectcv2.y -= 1
+        rectcv2.h += 1
+        pygame.display.set_mode([dx, 500])
--- view.py
+++ view.py
@@ -7,17 +7,26 @@ def cveta():
     g = random.randint(0, 255)
     b = random.randint(0, 255)
 
-a = pygame.display.set_mode([model.dx, 500])
 
+a = pygame.display.set_mode([model.dx, 500])
 cveta()
+
+cv1 = pygame.image.load("flower1.png")
+cv2 = pygame.image.load("flower2.png")
 kap = pygame.image.load("kaplya.png")
 
 
 def view1():
     global r, g, b
-    pygame.display.set_mode([model.dx, 500])
-    if model.rect.top > 499:
-        cveta()
+
+    a.fill([0, 0, 0])
     kap1 = pygame.transform.scale(kap, [model.rect.w, model.rect.h])
-    a.blit(kap1, [model.rect.x, model.rect.y])
+    if model.rectcv1.top != 0:
+        cv_1 = pygame.transform.scale(cv1, model.rectcv1.size)
+        cv_2 = pygame.transform.scale(cv2, model.rectcv2.size)
+    a.blit(cv_1, model.rectcv1)
+    a.blit(cv_2, model.rectcv2)
+    a.blit(kap1, model.rect)
+    pygame.draw.rect(a, [255, 255, 255], model.rectcv1, 2)
+    pygame.draw.rect(a, [0, 255, 255], model.rectcv2, 2)
     pygame.display.flip()
